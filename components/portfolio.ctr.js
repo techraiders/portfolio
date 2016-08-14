f704fa012877ecd7c77d3f9e263ce22907335007
techraiders <hello_navneet@hotmail.com>
2016-08-15 01:02:09 +0530

    Navbar and collapsible menu bar made responsive

--- components/portfolio.ctr.js
+++ components/portfolio.ctr.js
@@ -13,6 +13,16 @@
         $scope.services = data.data.services;
       })
     })
+    .controller("skillsCtrl", function($scope, $http) {
+      $http.get('data/data.json').then(function(data) {
+        $scope.skills = data.data.skills;
+      })
+    })
+    .controller("educationCtrl", function($scope, $http) {
+      $http.get('data/data.json').then(function(data) {
+        $scope.educations = data.data.educations;
+      })
+    })
     .controller("trainingsCtrl", function($scope, $http) {
         $http.get('data/data.json').then(function(data) {
           $scope.courses = data.data.trainings;
