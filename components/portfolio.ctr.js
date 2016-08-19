6d3b24697417abbcf096a9bc44e0bc2b9e4c2524
techraiders <hello_navneet@hotmail.com>
2016-08-20 04:16:55 +0530

    Adds new items in menu bar, adds slide show of achievements, improves design

--- components/portfolio.ctr.js
+++ components/portfolio.ctr.js
@@ -8,6 +8,11 @@
         $scope.summary = data.data.summary;
       })
     })
+    .controller("slidesCtrl", function($scope, $http) {
+      $http.get('data/data.json').then(function(response) {
+        $scope.images = response.data.slides;
+      })
+    })
     .controller("servicesCtrl", function($scope, $http) {
       $http.get('data/data.json').then(function(data) {
         $scope.services = data.data.services;
