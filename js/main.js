6d0d2c55828c7782edc0d2efc80abb832d7647e3
techraiders <hello_navneet@hotmail.com>
2016-08-21 01:03:41 +0530

    Adds share buttons on right in two different Sidenavigations with their open and close behavior

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+/*sidenav sharing icons opens and closes*/
+function close() {
+      document.getElementById("shareicon").style.display= "none";
+    }
+function open() {
+  document.getElementById("shareicon").style.display = "block";
+}
+
+
+function w3_openShareIconsSidenav() {
+    document.getElementById("shareiconsholder").style.display = "block";
+    close();
+}
+function w3_closeShareIconsSidenav() {
+    document.getElementById("shareiconsholder").style.display = "none";
+    open();
+}
 
