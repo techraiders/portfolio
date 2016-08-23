68552e032d7865b29450c8106bf547c087357484
techraiders <hello_navneet@hotmail.com>
2016-08-23 07:01:37 +0530

    Adds Google pie chart of skiils in a card model

--- scripts/portfolio.ctr.js
+++ scripts/portfolio.ctr.js
@@ -3,11 +3,11 @@
 
   angular
     .module("ngPortfolio")
-    .controller("summaryCtrl", function($scope, $http) {
+    /*.controller("summaryCtrl", function($scope, $http) {
       $http.get('data/data.json').then(function(data) {
         $scope.summary = data.data.summary;
       })
-    })
+    })*/
     .controller("slidesCtrl", function($scope, $http) {
       $http.get('data/data.json').then(function(response) {
         $scope.images = response.data.slides;
