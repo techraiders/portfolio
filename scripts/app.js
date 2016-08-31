24c181d2b8c0cd5ca57dad3e880612edf4ddf291
techraiders <hello_navneet@hotmail.com>
2016-08-31 18:14:36 +0530

    Data update in introduction

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-angular.module("ngPortfolio", []);
+angular.module("ngPortfolio", ['ui.router'])
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('stateone', {
+        url: '/stateone',
+        template: '<h1>State One'
+      })
+      .state('statetwo', {
+        url: '/statetwo',
+        template: '<h1>State Two'
+      });
+  });
