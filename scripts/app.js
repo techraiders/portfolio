5c3f467b3385462f37ec18aeb74e6cbd24d15d18
techraiders <hello_navneet@hotmail.com>
2016-09-01 03:15:38 +0530

    Bug fixes

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,1 @@
-angular.module("ngPortfolio", ['ui.router'])
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('stateone', {
-        url: '/stateone',
-        template: '<h1>State One'
-      })
-      .state('statetwo', {
-        url: '/statetwo',
-        template: '<h1>State Two'
-      });
-  });
+angular.module("ngPortfolio", []);
