aeea15d69ee0aacf55ebc4cb0e713d413e2fa062
techraiders <hello_navneet@hotmail.com>
2016-11-05 18:40:06 +0530

    Uses candidates factory to retrieve data which factory is later consumed by homeCtrl to serve data to different views

---
+++ scripts/home.ctr.js
@@ -0,0 +1,47 @@
+(function () {
+  "use strict";
+
+  angular
+    .module("ngPortfolio")
+
+    .controller("homeCtrl", function($scope, $http, candidatesFactory) {
+      candidatesFactory.getCandidates().then(function(response) {
+        //$scope.services = response.data.services;
+        $scope.navneet = response.data;
+        console.log("Navneet: ", $scope.navneet);
+      })
+
+      $scope.div = ['Progress Bars', 'Table'];
+      $scope.selectedDiv = $scope.div[0];
+
+      // Intializing the filter variable
+      $scope.sortColumn = '-experience', // - prefix operator sorts in decending order
+      $scope.reverseSort = false;
+      
+      $scope.sortData = function(column) {
+        $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+        $scope.sortColumn = column;
+
+      }
+
+      $scope.getSortClass = function(column) {
+        if ($scope.sortColumn == column) {
+          return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+        }
+      }
+
+    })
+
+     /*$scope.seconds = 0;
+     $scope.minutes = 0;
+      var countUp = function() {
+        $scope.seconds += 1;
+          if ($scope.seconds===60) {
+            $scope.minutes += 1;
+            $scope.seconds = 0;
+          };
+        $timeout(countUp, 1000);
+      }
+      $timeout(countUp, 1000);*/
+
+})();
