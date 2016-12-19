ebb08120ce618c30f1a9e069451206c7a80c3823
techraiders <hello_navneet@hotmail.com>
2016-12-19 21:42:12 +0530

    Adds Projects Showcase section in the portfolio, creates Pig-animation component, displays into the showcase using ui-view of ui-router.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 angular.module("ngPortfolio", ['ui.router', '720kb.socialshare'])
-	.config(function($urlRouterProvider, $urlMatcherFactoryProvider){
+	.config(function($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider){
       $urlMatcherFactoryProvider.caseInsensitive(true);
+
+      $stateProvider
+        .state('pig-animation', {
+          url: '/showcase',
+          templateUrl: 'components/pig-animation/pig06.html'
+        })
+        .state('portfolio', {
+        	url: '/portfolio',
+        	templateUrl: 'components/portfolio/portfolio04.html'
+        })
 
        $urlRouterProvider.otherwise('/');
     });
