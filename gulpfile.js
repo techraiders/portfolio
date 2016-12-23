54d99f2a67a2fd16c269ff8a297d0e182db850d6
techraiders <hello_navneet@hotmail.com>
2016-12-24 04:43:14 +0530

    Creates first gulp task 'manage images'.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,40 @@
 // Gulp.js configuration
 
 // include gulp and plugins
-var gulp = require('gulp');
+var
+	gulp = require('gulp'),
+	newer = require('gulp-newer'),
+	imagemin = require('gulp-imagemin'),
+	del = require('del');
 
+//file locations
 var
 	source = 'source/',
-	dest = 'build/';
+	dest = 'build/',
+
+	images = {
+		in: source + 'img/*.*',
+		out: dest + 'img/'
+	};
+
+// cleans the build folder
+gulp.task('clean', function() {
+	del([
+		dest + '*'
+	]);
+});
+
+// manages images
+gulp.task('images', function() {
+	return gulp.src(images.in)
+		.pipe(newer(images.out))
+		.pipe(imagemin())
+		.pipe(gulp.dest(images.out));
+});
+
 
 // default task
-gulp.task('default', function() {
-	
+gulp.task('default', ['images'], function() {
+	// images changes
+	gulp.watch(images.in, ['images']);
 });
