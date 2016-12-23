9f04085bbec606fd3c1db7cf635af392c6f8fde4
techraiders <hello_navneet@hotmail.com>
2016-12-24 04:22:58 +0530

    Installs gulp, creates package.json, and gulp.js

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// Gulp.js configuration
+
+// include gulp and plugins
+var gulp = require('gulp');
+
+var
+	source = 'source/',
+	dest = 'build/';
+
+// default task
+gulp.task('default', function() {
+	
+});
